59796e7f934153747ee7e148a78f0cf217711d81
김승주 <46523568+WWvision@users.noreply.github.com>
2022-12-01 23:46:51 +0900

    1201 업데이트
    -입퇴영 누르면 해당 정보 복사
    -히스토리에 새로운 정보 입력되면 새로 표시하기
    -기존 세션을 저장하고 초기값 불러오기
        1. 배차명령서 데이터 
        2. 입퇴영현황 세션 
        3. 히스토리 데이터
    - 입퇴영 현황에서 배차 목록 제거하는 버튼 만들기

--- OC_logic1.js
+++ OC_logic1.js
@@ -1,6 +1,6 @@
 var rowcount=1;
-var rowdata_Arr =  [];
-var car_obj_arr = [
+var rowdata_Arr =  [];//입력 폼 행 정보 배열
+var car_obj_arr = [//차량 정보 저장 배열
 	{ key: '1', val: '렉스턴, 1685-1' },
 	{ key: '5561', val: '스타렉스, 10육5561' },
 	{ key: '5510', val: '코란도, 10육5510' },
@@ -14,11 +14,21 @@ var car_obj_arr = [
 	{ key: '151', val: '마이티, 1685-151' },
 	{ key: '5562', val: '중형버스, 10육5562' }
 ];
-var TodayOfficeList = new Array();//히스토리를 위한 배열?
-//var TodayOfficeList_history = new Array();
-//console.log(car_obj_arr);
+var TodayOfficeList_arr = new Array();//히스토리를 위한 객체 배열
+/*var TodayOfficeList_arr = [{
+	bool: '여부',
+	time: '입퇴영시간',
+	name: '선탑자',
+	type: '운행종류',
+	carInfo: '차량정보',
+	destination: '목적지',
+	business: '용무',
+	remark: '비고'
+}];*/
+var final_arr = [];//입력폼에 있는 데이터를 가공한 뒤 저장하는 객체 배열
+//var Created_OfficerList_count=0;//입퇴영 현황에 생성된 div 갯수 > 자동저장하기 위함
 
-function addrow(){
+function addrow(){//입력 폼 행 추가
 	var OCtable = document.getElementById('OC_table');
 	var row = OCtable.insertRow(OCtable.rows.length);
 	var cell1 = row.insertCell(0);
@@ -39,7 +49,7 @@ function addrow(){
 	rowcount++;
 }
 
-function deleterow(){
+function deleterow(){//입력 폼 행 삭제
 	var OCtable = document.getElementById('OC_table');
 	if(rowcount==1){
 		alert("삭제할 항목이 없습니다!");
@@ -49,14 +59,12 @@ function deleterow(){
 	}
 }
 
-var final_arr = [];
-
-function send_data(){ //https://codingmoonkwa.tistory.com/12 참고해서 구현
+function send_data(){//입력 폼에 있는 데이터를 모두 불러와 원하는 정보로 가공
+	//https://codingmoonkwa.tistory.com/12 참고해서 구현
 	var table = document.getElementById("Operate_Car");
 	console.log("Operate_Car 값은 :" + table);
 	var counting_six = 0;
 	var data_arr=[];
-	var myObj = {};
 	console.log("테이블 요소 길이는 : " + table.length);
 	for(var count=0;count< table.length; count++){
 		console.log(count + "번째 테이블 요소 값은 : " + table.elements[count].value);
@@ -106,8 +114,11 @@ function send_data(){ //https://codingmoonkwa.tistory.com/12 참고해서 구현
 	}
 	var con = document.getElementById("input_box");
 	con.style.display = "none";
+	console.log("final_arr 출력");
 	console.log(final_arr);
-	make_officerList();
+	make_officerList(final_arr);//현재는 제출 폼밖에 없으므로 제출을 누르면 자동 실행
+	//이후엔 초기값이 설정되었는지 확인하고 안되어있으면 실행 & 추가하기만 실행되게끔
+	autoSave_InputForm();//배차명령서 데이터 저장
 }
 
 function search_carNum(val){//key값에 맞는 차량 번호를 리턴 
@@ -122,22 +133,26 @@ function search_carNum(val){//key값에 맞는 차량 번호를 리턴
 	if(returned == false) return "없는 번호";
 }
 
-function Going(div_id, button_id, index){
+function Going(div_id, button_id, index){//입퇴영 현황의 퇴영 버튼
 	document.getElementById(eval("'"+div_id+"'")).style.backgroundColor = "pink";
 	const btnElement = document.getElementById(eval("'"+button_id+"'"));
 	const times = new Date().toLocaleTimeString('ko-kr');
 	btnElement.value = "퇴영: " + times;
 	Save_ListData(index, times, "퇴영");
+	reloading_History();
+	autoSave_ViewBox();
 };
-function Coming(div_id, button_id, index){
+function Coming(div_id, button_id, index){//입퇴영 현황의 입영 버튼
 	document.getElementById(eval("'"+div_id+"'")).style.backgroundColor = "lightgreen";
 	const btnElement = document.getElementById(eval("'"+button_id+"'"));
 	const times = new Date().toLocaleTimeString('ko-kr');
 	btnElement.value = "입영: " + times;
 	Save_ListData(index, times, "입영");
+	reloading_History();
+	autoSave_ViewBox();
 };
 
-function Save_ListData(index, recordedTime, bool_type){
+function Save_ListData(index, recordedTime, bool_type){//입영, 퇴영 버튼을 누르면 해당 정보 저장
 	console.log("실행!");
 	var ListData = new Object();
 	
@@ -150,6 +165,104 @@ function Save_ListData(index, recordedTime, bool_type){
 	ListData.business = document.getElementById(eval("'ofc_business_"+index+"'")).value;
 	ListData.remark = document.getElementById(eval("'ofc_remark_"+index+"'")).value;
 
-	TodayOfficeList.push(ListData);
-	console.log(TodayOfficeList);
+	copyClipboard(ListData);//입력된 객체의 정보를 클립보드에 복사시키는 함수
+	TodayOfficeList_arr.push(ListData);//입퇴영 기록에 추가
+	console.log(TodayOfficeList_arr);
+	autoSave_HistoryList();
+}
+
+function copyClipboard(obj){//입영, 퇴영 버튼을 누르면 자동으로 해당 내용이 복사
+	//https://thinkforthink.tistory.com/341 참고
+	var text;
+	if(obj.bool == "입영"){// 선탑(차량, 차량번호)/통합막사/복귀
+		text =  obj.type + "(" + obj.carInfo + ")/통합막사/복귀";
+	} else if(obj.bool == "퇴영"){// 선탑(차량, 차량번호)/목적지/용무
+		text =  obj.type + "(" + obj.carInfo + ")/" + obj.destination + "/" + obj.business;
+	}
+	const textarea = document.createElement('textarea');
+	textarea.textContent = text;
+	document.body.append(textarea);
+	textarea.select();
+	document.execCommand('copy');
+	textarea.remove();
+}
+
+function delete_OfficerDiv(div_id, index){//입퇴영현황 Div 삭제 할 수 있는 함수
+	var del_name = document.getElementById(eval("'ofc_name_"+index+"'")).value;
+	var del_type = document.getElementById(eval("'ofc_type_"+index+"'")).value;
+	var del_carInfo = document.getElementById(eval("'ofc_carInfo_"+index+"'")).value;
+	var del_destination = document.getElementById(eval("'ofc_destination_"+index+"'")).value;
+	var del_business = document.getElementById(eval("'ofc_business_"+index+"'")).value;
+	var msg = del_name + "/" + del_type + "(" + del_carInfo + ")/" + del_destination + "/" + del_business;
+	if(confirm(msg + "를 정말 삭제하시겠습니까?")){
+		var parent = document.getElementById("view_box");
+		var child = document.getElementById(eval("'"+ div_id +"'"));
+		parent.removeChild(child);
+		autoSave_ViewBox();
+	} else ;
+}
+
+
+
+
+//저장해야 되는거 - 배차명령서 입력폼 데이터 /입퇴영현황 세션/히스토리 데이터
+function autoSave_InputForm(){//자동저장 - 배차명령서 입력폼
+	localStorage.setItem("AllocateCar_data", JSON.stringify(final_arr));
+	console.log("배차명령서 데이터가 저장되었습니다");
+}
+function loadData_InputForm(){//초기값 불러오기 - 배차명령서 입력폼
+	console.log("배차명령서 데이터 불러오기");
+	var InputForm_data = localStorage.getItem("AllocateCar_data");
+	if(InputForm_data != null){
+		final_arr = JSON.parse(InputForm_data);
+		//make_officerList(final_arr);
+	}
+}
+function autoSave_HistoryList(){//자동저장 - 히스토리 데이터
+	localStorage.setItem("HistoryList_data", JSON.stringify(TodayOfficeList_arr));
+	console.log("입퇴영기록 데이터가 저장되었습니다");
+}
+function loadData_HistoryList(){//초기값 불러오기 - 히스토리 데이터  
+	var History_data = localStorage.getItem("HistoryList_data");
+	if(History_data != null){
+		TodayOfficeList_arr = JSON.parse(History_data);
+		console.log("입퇴영기록 데이터 불러오기");
+		console.log(TodayOfficeList_arr);
+		make_historyList();
+	}
+}
+
+var SessionDiv_arr = new Array();//세션 배열
+function autoSave_ViewBox(){//자동저장 - 입퇴영현황 세션
+	var MaxCount = document.getElementById('view_box').childElementCount - 1;
+	if(SessionDiv_arr != 0){
+		SessionDiv_arr = [];
+	}
+	for(var count=0; count < MaxCount;count++){
+		var Session_obj = {};
+		//%%Session_obj.div_color = document.getElementById(eval("'officerList"+count+"'")).value;
+		Session_obj.coming_btn = document.getElementById(eval("'coming_"+count+"'")).value;
+		Session_obj.going_btn = document.getElementById(eval("'going_"+count+"'")).value;
+		Session_obj.name_inp = document.getElementById(eval("'ofc_name_"+count+"'")).value;
+		Session_obj.type_inp = document.getElementById(eval("'ofc_type_"+count+"'")).value;
+		Session_obj.carInfo_inp = document.getElementById(eval("'ofc_carInfo_"+count+"'")).value;
+		Session_obj.destination_inp = document.getElementById(eval("'ofc_destination_"+count+"'")).value;
+		Session_obj.business_inp = document.getElementById(eval("'ofc_business_"+count+"'")).value;
+		Session_obj.remark_inp = document.getElementById(eval("'ofc_remark_"+count+"'")).value;
+		console.log(count + "번째 세션 :");
+		console.log(Session_obj);
+		SessionDiv_arr.push(Session_obj);
+	}
+	localStorage.setItem("SessionDiv_data", JSON.stringify(SessionDiv_arr));
+	console.log("현재 세션 데이터가 저장되었습니다");
+}
+
+function loadData_ViewBox(){//초기값 불러오기 - 입퇴영현황 세션 
+	var Session_data = localStorage.getItem("SessionDiv_data");
+	if(Session_data != null){
+		SessionDiv_arr = JSON.parse(Session_data);
+		console.log("세션 데이터 불러오기");
+		console.log(SessionDiv_arr);
+		make_officerList(SessionDiv_arr);
+	}
 }
