d5c01c1b3e0b28b54d6f616293161b7272d56ece
김승주 <46523568+WWvision@users.noreply.github.com>
2023-01-05 23:41:27 +0900

    0105 업데이트
    민간인출입통제 디자인 및 정리 끝

---
+++ Visitor_part/Visitor_part_logic2.js
@@ -0,0 +1,8 @@
+function toggleAddVisitor(){
+    let con = document.getElementById("Visitor_Input_Area");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    }else{
+        con.style.display = "none";
+    }
+}
